0f2158f18b8b2f47558aa54fe3ab44c28a726cd4
gabiiiflores <unigabiroxo11flores@gmail.com>
2024-10-01 11:44:27 -0400

    instalando cors

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import express from 'express'
 import mysql from 'mysql2/promise'
+import cors from 'cors'
 const app = express()
+app.use(express.json())
+app.use(cors())
 app.get("/produtos", async(req,res)=>{
     //OK -> 0 - Criar o banco de dados e iniciar o servidor de banco.
     //1 - Criar a conexão com o banco
