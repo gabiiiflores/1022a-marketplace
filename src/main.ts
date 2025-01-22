f0e5abadae1a36a2ab57870b164dc3f9ee46afe5
gabiiiflores <116742963+gabiiiflores@users.noreply.github.com>
2025-01-22 11:23:51 -0400

    atualizando

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import mysql from 'mysql2/promise'
 import cors from 'cors'
 import BancoMysql from './db/banco-mysql'
+import BancoMongo from './db/banco-mongo'
 
 const app = express()
 app.use(express.json())
@@ -22,6 +23,7 @@ app.get("/produtos", async (req, res) => {
 })
 app.get("/produtos/:id", async (req, res) => {
     try {
+        
         const banco = new BancoMysql()
         await banco.criarConexao()
         const result = await banco.listarPorId(req.params.id)
@@ -37,7 +39,7 @@ app.post("/produtos", async (req, res) => {
         const {id,nome,descricao,preco,imagem} = req.body
         const banco = new BancoMysql()
         await banco.criarConexao()
-        const produto = {id,nome,descricao,preco,imagem}
+        const produto = {id:parseInt(id),nome,descricao,preco,imagem}
         const result = await banco.inserir(produto)
         await banco.finalizarConexao()
         res.send(result) 
@@ -50,21 +52,22 @@ app.post("/produtos", async (req, res) => {
 //DELETAR
 app.delete("/produtos/:id",async(req,res)=>{
     try{
-    const banco = new BancoMysql()
-    await banco.criarConexao()
-    const result = await banco.excluir(req.params.id)
-    await banco.finalizarConexao()
-    res.status(200).send("Produto excluido com sucesso id: "+req.params.id)
-}
-catch(e){
-    console.log(e)
-    res.status(500).send("Erro ao excluir")
-}
+        const banco = new BancoMysql()
+        await banco.criarConexao()
+        const result = await banco.excluir(req.params.id)
+        await banco.finalizarConexao()
+        res.status(200).send("Produto excluido com sucesso id: "+req.params.id)
+    }
+    catch(e){
+        console.log(e)
+        res.status(500).send("Erro ao excluir")
+    }
+    
 })
 
 //ALTERAR
 app.put("/produtos/:id",async(req,res)=>{
-    const {id,nome,descricao,preco,imagem} = req.body
+    const {nome,descricao,preco,imagem} = req.body
     const produto = {nome,descricao,preco,imagem}
     const banco = new BancoMysql()
     await banco.criarConexao()
